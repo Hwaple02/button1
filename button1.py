44f3fbb327fddd9e30650817e6fefefef9fd9296
Hangyeol Kang <benkang1624@gmail.com>
2023-09-12 22:38:37 +0900

    print Korean name

--- button1.py
+++ button1.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Hangyeol Kang")
+b1 = Button(window, text="강한결")
 b2 = Button(window, text="60213026")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
